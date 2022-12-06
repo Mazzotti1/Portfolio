12790f6752efdd396f558d2318192cbd4d65e11f
Gabriel Mazzotti <gmazzottidesouza@gmail.com>
2022-12-06 15:33:11 -0300

    adcionado portifolio pessoal

---
+++ assets/script/projetos.js
@@ -0,0 +1,48 @@
+// Make Cards
+const sectionCards = document.querySelector("section.cards");
+const card = document.querySelector("div.card");
+
+
+const gifs = [
+  {
+    title: "Jogo da memoria com Html, JS puro e CSS Animation <br><br>",
+    thumb: "./assets/icons/jogo da memoria.gif",
+    gif_id: "1",
+    git:"https://github.com/Mazzotti1/Jogo-da-memoria",
+    link:"https://jogo-da-memoria-liard.vercel.app/",
+
+  },
+  {
+    title: "Jogo da velha com HTML, Css e JS <br><br><br>",
+    thumb: "./assets/icons/jogo da velha.gif",
+    gif_id: "2",
+    git:"https://github.com/Mazzotti1/JogoDaVelha",
+    link:"https://jogo-da-velha-plum-phi.vercel.app/"
+  },
+  {
+    title: "Projeto em desenvolvimento, procurando evoluir sempre!",
+    thumb: "./assets/icons/build.png",
+    gif_id: "3",
+    link:"#home",
+    git:"#home"
+  },
+
+];
+
+
+gifs.map(gif => {
+  const cardClone = card.cloneNode(true);
+  cardClone.setAttribute("id", gif.gif_id);
+  cardClone.querySelector("img").src = gif.thumb;
+  cardClone.querySelector(".title").innerHTML = gif.title;
+  cardClone.querySelector(".linkGit").href = gif.git;
+  cardClone.querySelector(".linkCard").href = gif.link;
+  sectionCards.appendChild(cardClone);
+
+});
+
+
+card.remove();
+
+
+
