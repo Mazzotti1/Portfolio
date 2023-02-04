bafa94728598a91c2817aec496bf93300a627286
Gabriel Mazzotti <gmazzottidesouza@gmail.com>
2023-02-04 19:04:38 -0300

    Adcionado gap na foto do about

--- src/Components/AboutMe/AboutMe.tsx
+++ src/Components/AboutMe/AboutMe.tsx
@@ -13,8 +13,8 @@ export const AboutMe: React.FC<Props> = ({language }) => {
     return(
         <ContainerAbout>
         <div id="About" className=" h-full flex justify-center   ">
-           <div className="pl-52 pr-52 containerResume flex justify-center items-center  " >
-            <img  src={perfil} className=" imgAbout h-1/5 rounded-3xl shadow-md hover:shadow-gray-400  hover:scale-110  transition-all " data-anime="top" />
+           <div className="pl-52 pr-52 containerResume flex justify-center items-center gap-8 " >
+            <img  src={perfil} className=" imgAbout  h-1/5 rounded-3xl shadow-md hover:shadow-gray-400  hover:scale-110  transition-all " data-anime="top" />
 
             <div className=" containerText flex  flex-col">
             <h1 className="text-fonteVerde text-4xl mb-5 ">{language === 'pt' ? pt.TittleAbout : en.TittleAbout}</h1>
